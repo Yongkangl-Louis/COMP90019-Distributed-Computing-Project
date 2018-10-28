5bcc73db7b5327cadd4ae43e289bc25feb9f9ea4
Yongkangl-Louis <39141451+Yongkangl-Louis@users.noreply.github.com>
2018-10-28 14:25:26 +1100

    Update view_creation2.py

--- automation/zip/view_creation2.py
+++ automation/zip/view_creation2.py
@@ -1,7 +1,7 @@
 import couchdb
 from couchdb.design import ViewDefinition #abc
 
-couch = couchdb.Server('http://user:password@127.0.0.1:5984/')
+couch = couchdb.Server('http://admin:password@115.146.85.196:5984/')
 database = couch['mosttweets']
 
 view = ViewDefinition('toptweets', 'tweets', 'function (doc) { emit(doc.user_id, doc.user_id);}')

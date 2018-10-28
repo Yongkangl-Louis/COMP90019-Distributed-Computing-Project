a0c8086c66d0a62f0ea5944e5743d8665b20d113
Yongkangl-Louis <39141451+Yongkangl-Louis@users.noreply.github.com>
2018-10-28 14:28:41 +1100

    Update distance2.py

--- automation/zip/distance2.py
+++ automation/zip/distance2.py
@@ -10,7 +10,7 @@ import couchdb
 import numpy as np
 
 
-couch = couchdb.Server('http://user:password@115.146.85.196:5984/')
+couch = couchdb.Server('http://admin:password@115.146.85.196:5984/')
 data1 = couch['distance']
 
 result = data1.view('topuser/distance')

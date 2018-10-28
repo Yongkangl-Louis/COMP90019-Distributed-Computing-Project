be8e0bfbdfcf92f87b8baeef1e97fe4602ba0474
Yongkangl-Louis <39141451+Yongkangl-Louis@users.noreply.github.com>
2018-10-28 14:24:58 +1100

    Update view_creation1.py

--- automation/zip/view_creation1.py
+++ automation/zip/view_creation1.py
@@ -1,7 +1,7 @@
 import couchdb
 from couchdb.design import ViewDefinition #abc
 
-couch = couchdb.Server('http://user:password@127.0.0.1:5984/')
+couch = couchdb.Server('http://admin:password@115.146.85.196:5984/')
 database = couch['mostfollowers']
 
 topfollowers = ViewDefinition('topfollowers', 'followers', 'function (doc) { if(doc.user_id) {emit(doc.followers, doc.user_id);}}')

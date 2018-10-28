f61f8064b7453e594530c0c6b51dccded531a735
Yongkangl-Louis <39141451+Yongkangl-Louis@users.noreply.github.com>
2018-10-28 14:28:26 +1100

    Update distance1.py

--- automation/zip/distance1.py
+++ automation/zip/distance1.py
@@ -10,7 +10,7 @@ import sys
 import couchdb
 
 #database name and server url
-database_server = 'http://user:password@115.146.85.196:5984/'
+database_server = 'http://admin:password@115.146.85.196:5984/'
 couch = couchdb.Server(database_server)
 
 database_name ='distance'
